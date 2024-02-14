2be8cb65f5255a23edd484e84f471d1f3ab891f9
Abdullah Aly <abdullahaly.dev@gmail.com>
2024-02-14 05:41:50 +0200

    delete dist directory

--- dist/jest.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    rootDir: "./test",
-    testTimeout: 30 * 1000,
-};
-exports.default = config;
--- dist/source/cores/roblox.core.d.ts
+++
@@ -1,6 +0,0 @@
-export default class RobloxCore {
-    static buildRobloxPath(): string;
-    static isRobloxInstalled(): boolean;
-    static findClientVersionPath(): string;
-    static setClientAppSettings(): void;
-}
